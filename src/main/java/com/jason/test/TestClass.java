050d432c34cb4bfbc0b84cf3b87d586ecefacaf8
raojiasong <raojssh@sinosoft.com.cn>
2018-01-29 19:38:51 +0800

    第一次提交代码

---
+++ src/main/java/com/jason/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.jason.test;
+
+public class TestClass {
+
+	private static String getName(String name) {
+		return name;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getName("Lucy"));
+	}
+}
